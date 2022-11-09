4d12c54400d10286d70c121db3e4b48849c00a39
Alin Cola <colaalincristian@gmail.com>
2022-11-09 15:57:59 +0200

    javascript optimisierung

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function showPassword(button) {
-    let password = document.getElementById("password");
-    if (password.type === "password") {
-        button.innerHTML = "Hide Password";
-        password.type = "text";
-    } else {
-        button.innerHTML = "Show Password";
-        password.type = "password";
-    }
+const password = document.querySelector("#password");
+const btn = document.querySelector("#show")
+
+function showPassword() {
+     if (password.type === "password") {
+         btn.innerText = "Hide Password";
+         password.type = "text";
+     } else {
+         btn.innerText = "Show Password";
+         password.type = "password";
+     }
 }
+btn.addEventListener("click", showPassword);
+
